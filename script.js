096bf52e3cd135d795ee7ffee697e87c026a78cf
Jovuto <153099429+Jovuto@users.noreply.github.com>
2024-10-07 12:05:36 +0100

    feat: add toggleRead button and add more example books

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const isFinishedDiv = document.querySelector(".isFinishedDiv");
 const isReadCheckbox = document.querySelector("#isRead");
 const isFinishedCheckbox = document.querySelector("#isFinished");
 const deleteButton = document.querySelectorAll(".deleteButton");
+const readButton = document.querySelectorAll(".readButton");
 
 showDialogue.addEventListener("click", () => {
     formDialogue.showModal();
@@ -24,6 +25,11 @@ function Book(name, author, pages, isRead) {
     this.isRead = isRead;
 }
 
+Book.prototype.toggleRead = function() {
+    this.isRead = !this.isRead;
+    refreshLibrary();
+}
+
 function Manga(name, author, volumes, isFinished, isRead) {
     this.name = name;
     this.author = author;
@@ -32,11 +38,20 @@ function Manga(name, author, volumes, isFinished, isRead) {
     this.isRead = isRead;
 }
 
+Object.setPrototypeOf(Manga.prototype, Book.prototype);
+
 const onePiece = new Manga("One Piece", "Eiichiro Oda", 109, false, false);
 const caseClosed = new Manga("Case Closed", "Gosho Aoyama", 105, false, false);
 const dragonBall = new Manga("Dragon Ball", "Akira Toriyama", 42, true, true);
+const naruto = new Manga("Naruto", "Masashi Kishimoto", 72, true, true);
+const aTaleOfTwoCities = new Book ("A Tale of Two Cities", "Charles Dickens", 544, false);
+const theLittlePrince = new Book ("The Little Prince", "Antoine de Saint-Exupéry", 109, false);
+const slamDunk = new Manga ("Slam Dunk", "Takehiko Inoue", 31, true, false);
+const theAlchemist = new Book ("The Alchemist", "Paulo Coelho", 208, false);
+const andThenThereWereNone = new Book ("And Then There Were None", "Agatha Christie", 240, false);
+const dreamOfTheRedChamber = new Book ("Dream of the Red Chamber", "Cao Xueqin", 352, false);
 
-
+console.log(Object.getPrototypeOf(Manga));
 
 function addBookToLibrary(book) {
 
@@ -47,13 +62,24 @@ function addBookToLibrary(book) {
 addBookToLibrary(onePiece);
 addBookToLibrary(caseClosed);
 addBookToLibrary(dragonBall);
+addBookToLibrary(naruto);
+addBookToLibrary(aTaleOfTwoCities);
+addBookToLibrary(theLittlePrince);
+addBookToLibrary(slamDunk);
+addBookToLibrary(theAlchemist);
+addBookToLibrary(andThenThereWereNone);
+addBookToLibrary(dreamOfTheRedChamber);
 
 refreshLibrary();
 
 pageContent.addEventListener("click", (e) => {
     if (e.target.classList.contains("deleteButton")){
-        console.log("Delete button clicked!")
-        removeBook(e.target.dataset.ArrayIndex);
+        console.log(e.target.dataset.arrayIndex);
+        removeBook(e.target.dataset.arrayIndex);
+    }
+    else if (e.target.classList.contains("readButton")) {
+        console.log(e.target.dataset.arrayIndex);
+        myLibrary[e.target.dataset.arrayIndex].toggleRead();
     }
 })
 
@@ -70,7 +96,8 @@ function refreshLibrary() {
                 <p>Author: ${book.author}</p>
                 <p>Pages: ${book.pages}</p>
                 <p>Read: ${book.isRead}</p>
-                <button class="deleteButton" data-array-index='${i}'>Delete</button>
+                <button class="deleteButton" data-array-index=${i}>Delete</button>
+                <button class="readButton" data-array-index=${i}>Toggle Read</button>
             </div>`);
         }
         else {
@@ -81,7 +108,8 @@ function refreshLibrary() {
                 <p>Volumes: ${book.volumes}</p>
                 <p>Series finished: ${book.isFinished}</p>
                 <p>Read: ${book.isRead}</p>
-                <button class="deleteButton" data-array-index='${i}'>Delete</button>
+                <button class="deleteButton" data-array-index=${i}>Delete</button>
+                <button class="readButton" data-array-index=${i}>Toggle Read</button>
             </div>`)
             console.log("manga printed");
         }
